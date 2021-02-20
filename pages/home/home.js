dde244ac2efc87f635ab7276e0e1f79a2db9af6b
Frank-1997-wyf <74478042+Frank-1997-wyf@users.noreply.github.com>
2021-02-20 11:32:00 +0800

    常用标签

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,19 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Frank',
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  getInfo(e) {
+    console.log(e);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtn() {
+    // 1.数据发生变化，但界面不会刷新
+    // this.data.counter++;
+    // console.log(this.data.counter);
+    console.log(this);
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1,
+    });
   }
 })
