b83139ec348c222b1006c323b568e9a1b507654d
Frank-1997-wyf <74478042+Frank-1997-wyf@users.noreply.github.com>
2021-02-21 23:49:32 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,55 +14,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 原生发送网络请求
+    // this.get_data_origin();
 
+    // 使用封装的request发送网络请求
+    request({
+      url: 'http://152.136.185.210:7878/api/m5/home/multidata'
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin() {
+    // 发送网络请求
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "post",
+    //   data: {
+    //     name: 'frank',
+    //     age: 24
+    //   },
+    //   success(res) {
+    //     console.log(res);
+    //   },
+    //   fail(err) {
+    //     console.log(err);
+    //   }
+    // })
   }
 })
